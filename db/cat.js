e40e6dad4003084df76ae5a466c415817d65f8e1
Joe-Pena <joepena@protonmail.com>
2018-11-16 10:02:42 -0700

    Modify get endpoints to get first item and add delete endpoints for cat and dog

--- db/cat.js
+++ db/cat.js
@@ -10,6 +10,28 @@ const Cat = [
     age: 2,
     breed: 'Bengal',
     story: 'Thrown on the street'
+  },
+  {
+    imageURL:
+      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription:
+      'Orange cat that likes lasagna',
+    name: 'Garfield',
+    sex: 'Male',
+    age: 27,
+    breed: 'Orange Cat',
+    story: 'Owner got tired of his eating habits'
+  },
+  {
+    imageURL:
+      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription:
+      'White siamese with good business taste',
+    name: 'Princess Twinkle Toes',
+    sex: 'Female',
+    age: 1,
+    breed: 'Siamese',
+    story: 'Ex strip club owner'
   }
 ];
 
--- db/dog.js
+++ db/dog.js
@@ -10,6 +10,28 @@ const Dog = [
     age: 3,
     breed: 'Golden Retriever',
     story: 'Owner Passed away'
+  },
+  {
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription:
+      'A big black lab',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Black Labrador',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription:
+      'A smiling handsome overweight bulldog',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Bulldog',
+    story: 'pageant dog'
   }
 ];
 
