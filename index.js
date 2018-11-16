80ab2202cdd40193659cd0c869e551bd9ed989ee
clkent <chelsea.kent@gmail.com>
2018-11-16 08:43:57 -0800

    Set up initial GET and DELETE endpoints for dogs and cats

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
+//const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
+
+//cat mock db data
+const Cat = require('./db/cat');
+//dog mock db data
+const Dog = require('./db/dog');
 
 const app = express();
 
@@ -22,6 +27,20 @@ app.use(
   })
 );
 
+//GET Endpoint
+
+//Cat
+app.get('/api/cat', (req, res, next) => {
+  return res.json(Cat);
+});
+
+//Dog
+app.get('/api/dog', (req, res, next) => {
+  return res.json(Dog);
+});
+
+//DELETE Endpoint
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
@@ -34,7 +53,7 @@ function runServer(port = PORT) {
 }
 
 if (require.main === module) {
-  dbConnect();
+  // dbConnect();
   runServer();
 }
 
