76d9551cad055617ef066cd65c8d5bddd77f4ca7
clkent <chelsea.kent@gmail.com>
2018-11-17 09:43:31 -0800

    adding more doggos and kittiez

--- db/cat.js
+++ db/cat.js
@@ -4,8 +4,7 @@ const Queue = require('./queue');
 const Cat = new Queue();
 
 Cat.enqueue({
-  imageURL:
-    'https://imgur.com/KGCT9mw.jpg',
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
   imageDescription:
     'Orange bengal cat with black stripes lounging on concrete.',
   name: 'Fluffy',
@@ -14,23 +13,46 @@ Cat.enqueue({
   breed: 'Bengal',
   story: 'Thrown on the street'
 });
-Cat.enqueue(
-  {
-    imageURL:
-      'https://imgur.com/w2cH7ml.jpg',
-    imageDescription:
-      'Orange cat that likes lasagna',
-    name: 'Garfield',
-    sex: 'Male',
-    age: 27,
-    breed: 'Orange Cat',
-    story: 'Owner got tired of his eating habits'
-  });
 Cat.enqueue({
-  imageURL:
-    'https://imgur.com/vZA6hsO.jpg',
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
   imageDescription:
-    'White siamese with good business taste',
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
   name: 'Princess Twinkle Toes',
   sex: 'Female',
   age: 1,
@@ -38,5 +60,207 @@ Cat.enqueue({
   story: 'Ex strip club owner'
 });
 
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
+
+Cat.enqueue({
+  imageURL: 'https://imgur.com/KGCT9mw.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/w2cH7ml.jpg',
+  imageDescription: 'Orange cat that likes lasagna',
+  name: 'Garfield',
+  sex: 'Male',
+  age: 27,
+  breed: 'Orange Cat',
+  story: 'Owner got tired of his eating habits'
+});
+Cat.enqueue({
+  imageURL: 'https://imgur.com/vZA6hsO.jpg',
+  imageDescription: 'White siamese with good business taste',
+  name: 'Princess Twinkle Toes',
+  sex: 'Female',
+  age: 1,
+  breed: 'Siamese',
+  story: 'Ex strip club owner'
+});
 
 module.exports = Cat;
--- db/dog.js
+++ db/dog.js
@@ -3,10 +3,8 @@ const Queue = require('./queue');
 
 const DogList = [
   {
-    imageURL:
-      'https://imgur.com/YLkMx9d.jpg',
-    imageDescription:
-      'A smiling golden-brown winnie dog listening to music.',
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
     name: 'Zeus',
     sex: 'Male',
     age: 3,
@@ -14,10 +12,8 @@ const DogList = [
     story: 'Owner Passed away'
   },
   {
-    imageURL:
-      'https://imgur.com/wJHGeYI.jpg',
-    imageDescription:
-      'A big gray pug',
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
     name: 'Poseidon',
     sex: 'Male',
     age: 3,
@@ -25,10 +21,467 @@ const DogList = [
     story: 'Ex security guard'
   },
   {
-    imageURL:
-      'https://imgur.com/t8JjeOn.jpg',
-    imageDescription:
-      'A smiling handsome overweight chihuahua',
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
+    name: 'Honey Boo Boo',
+    sex: 'Female',
+    age: 2,
+    breed: 'English Chihuahua',
+    story: 'pageant dog'
+  },
+  {
+    imageURL: 'https://imgur.com/YLkMx9d.jpg',
+    imageDescription: 'A smiling golden-brown winnie dog listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Winnie Dog',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'https://imgur.com/wJHGeYI.jpg',
+    imageDescription: 'A big gray pug',
+    name: 'Poseidon',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pug',
+    story: 'Ex security guard'
+  },
+  {
+    imageURL: 'https://imgur.com/t8JjeOn.jpg',
+    imageDescription: 'A smiling handsome overweight chihuahua',
     name: 'Honey Boo Boo',
     sex: 'Female',
     age: 2,
@@ -39,7 +492,7 @@ const DogList = [
 
 const Dog = new Queue();
 
-for(let doggo of DogList) {
+for (let doggo of DogList) {
   Dog.enqueue(doggo);
 }
 
